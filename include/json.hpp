5efbb25484ece75dc33fefd1afb71103a845921b
Daniel Sipka <no1msd@gmail.com>
2015-10-12 12:18:35 +0200

    missing include

--- include/json.hpp
+++ include/json.hpp
@@ -1,4 +1,5 @@
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/qi_rule.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_fusion.hpp>
